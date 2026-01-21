ea488850622f68f577d77fd01cddca280499814d
Igor Santos <igorlimasantos667@gmail.com>
2026-01-20 21:55:02 -0300

    create

---
+++ app.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect('bd.db')
+cursor = conn.cursor()
+cursor.execute('CREATE TABLE users (name TEXT, age INTEGER)')
+name = 'Igor'
+age = 27
+cursor.execute("INSERT INTO users VALUES (?, ?)",(name , age))
+conn.commit()
